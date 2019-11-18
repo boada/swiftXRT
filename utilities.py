07fe17ca68150debc200da0375c3d4ae5f95c6cf
Steven Boada <stevenboada@gmail.com>
2019-11-18 15:25:48 -0500

    yapf

--- utilities.py
+++ utilities.py
@@ -1,32 +1,45 @@
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from tqdm import tqdm_notebook
 
-def parallel_process(array, function, n_jobs=None, use_kwargs=False, front_num=0):
+
+def parallel_process(array,
+                     function,
+                     n_jobs=None,
+                     use_kwargs=False,
+                     front_num=0):
     """
         A parallel version of the map function with a progress bar.
 
         Args:
             array (array-like): An array to iterate over.
-            function (function): A python function to apply to the elements of array
-            n_jobs (int, default=16): The number of cores to use
-            use_kwargs (boolean, default=False): Whether to consider the elements of array as dictionaries of
-                keyword arguments to function
-            front_num (int, default=3): The number of iterations to run serially before kicking off the
-                parallel job. This can be useful for catching bugs
+            function (function): A python function to apply to the elements of
+            array n_jobs (int, default=16): The number of cores to use
+            use_kwargs (boolean, default=False): Whether to consider the
+            elements of array as dictionaries of keyword arguments to function
+            front_num (int, default=3): The number of iterations to run
+            serially before kicking off the parallel job. This can be useful
+            for catching bugs
         Returns:
             [function(array[0]), function(array[1]), ...]
+
     """
-    #We run the first few iterations serially to catch bugs
+
+    # We run the first few iterations serially to catch bugs
     if front_num > 0:
-        front = [function(**a) if use_kwargs else function(a) for a in array[:front_num]]
-    #If we set n_jobs to 1, just run a list comprehension. This is useful for benchmarking and debugging.
-    if n_jobs==1:
-        out = [function(**a) if use_kwargs else function(a) for a in tqdm_notebook(array[front_num:])]
+        front = [
+            function(**a) if use_kwargs else function(a)
+            for a in array[:front_num]
+        ]
+
+    # If we set n_jobs to 1, just run a list comprehension.
+    # This is useful for benchmarking and debugging.
+    if n_jobs == 1:
+        out = [
+            function(**a) if use_kwargs else function(a)
+            for a in tqdm_notebook(array[front_num:])
+        ]
         return
-#         if front_num:
-#             return front + out
-#         else:
-#             return out
+
     #Assemble the workers
     with ProcessPoolExecutor(max_workers=n_jobs) as pool:
         #Pass the elements of array into function
@@ -37,20 +50,21 @@ def parallel_process(array, function, n_jobs=None, use_kwargs=False, front_num=0
         kwargs = {
             'total': len(futures),
             'unit': 'it',
-            'unit_scale': True,
+            'unit_scale': False,
             'leave': True
         }
-        #Print out the progress as tasks complete
+        # Print out the progress as tasks complete
         for f in tqdm_notebook(as_completed(futures), **kwargs):
             pass
-#     out = []
-#     #Get the results from the futures.
-#     for i, future in tqdm_notebook(enumerate(futures)):
-#         try:
-#             out.append(future.result())
-#         except Exception as e:
-#             out.append(e)
-#     if front_num:
-#         return front + out
-#     else:
-#         return out
+
+        # out = []
+        # # Get the results from the futures.
+        # for i, future in tqdm_notebook(enumerate(futures)):
+        #     try:
+        #         out.append(future.result())
+        #     except Exception as e:
+        #         out.append(e)
+        # if front_num:
+        #     return front + out
+        # else:
+        #     return out
